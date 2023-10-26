da99b3de21b98dae5cd2fff0a0e5773853fcc2a3
CudraMohamed <ncudramohamed@gmail.com>
2023-10-26 15:29:35 +0300

    implimented front-end

--- src/Footer.js
+++ src/Footer.js
@@ -12,10 +12,10 @@ const Footer = () => {
             <a href="https://web.facebook.com/login/?_rdc=1&_rdr" className="text-white hover:text-gray-400 mx-2">
               Facebook
             </a>
-            <a href="#" className="text-white hover:text-gray-400 mx-2">
+            <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D" className="text-white hover:text-gray-400 mx-2">
               Twitter
             </a>
-            <a href="#" className="text-white hover:text-gray-400 mx-2">
+            <a href="https://www.instagram.com/accounts/login/" className="text-white hover:text-gray-400 mx-2">
               Instagram
             </a>
           </div>
